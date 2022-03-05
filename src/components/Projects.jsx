cfe597b1ae710283534be82bbb5d18307620f0bd
sarry7045 <sarry7045@gmail.com>
2022-03-05 22:04:42 +0530

    Chages

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,6 +10,7 @@ import { RiVideoChatLine } from "react-icons/ri";
 import { TiWeatherCloudy } from "react-icons/ti";
 import { GiNotebook } from "react-icons/gi";
 import { BsTextareaT } from "react-icons/bs";
+import { AiFillGoogleCircle } from "react-icons/ai";
 import Zoom from "react-reveal/Zoom";
 import Helmet from "react-helmet";
 
@@ -20,62 +21,69 @@ const Services = () => {
   const [state] = React.useState([
     {
       id: 1,
+      icon: <AiFillGoogleCircle className="commonIcons" />,
+      heading: "Google Clone",
+      text: "In this Project i created one Basic Google clone with some search Functionality using Rapid API.",
+      href: "https://google-clonee.netlify.app/",
+    },
+    {
+      id: 2,
       icon: <MdAppRegistration className="commonIcons" />,
       heading: "Login Registration Form",
       text: "In this Project i created one basic Registraion and Login Form (Only Frontend Part Done).",
       href: "https://mern-client-side.netlify.app/",
     },
     {
-      id: 2,
+      id: 3,
       icon: <AiOutlineForm className="commonIcons" />,
       heading: "Multi-Step Form",
       text: "In this Project i Created Multistep Form Using Formik and Some Functionality.",
       href: "https://multistepp-form.netlify.app/",
     },
     {
-      id: 3,
+      id: 4,
       icon: <RiVideoChatLine className="commonIcons" />,
       heading: "Video Chat App",
       text: "In this Project i created one Video Chat app using Socket.IO and Material-UI.",
       href: "https://videeo-chatt-app.netlify.app/",
     },
     {
-      id: 4,
+      id: 5,
       icon: <AiOutlineShoppingCart className="commonIcons" />,
       heading: "React Add-To-Cart",
       text: "In this Project i created one Add to Cart aap With Some Funcationality.",
       href: "https://shoppingg-caart.netlify.app/",
     },
     {
-      id: 5,
+      id: 6,
       icon: <MdOutlineCoronavirus className="commonIcons" />,
       heading: "Covid19 Tracker",
       text: "In this Project i created one Covid 19 Cases Tracker App with Graph Representation.",
       href: "https://coviid19-tracker.netlify.app/",
     },
     {
-      id: 6,
+      id: 7,
       icon: <AiOutlineForm className="commonIcons" />,
       heading: "React-Formik",
       text: "In this Project i Created 4 Registration Forms Using React Formik.",
       href: "https://reactt-formik.netlify.app/",
     },
     {
-      id: 7,
+      id: 8,
       icon: <MdOutlineFoodBank className="commonIcons" />,
       heading: "React Restaurant Web",
       text: "In this Project i created one Restaurant type Website with some categories.",
       href: "https://surajreactrestaurant.netlify.app/",
     },
     {
-      id: 8,
+      id: 9,
       icon: <TiWeatherCloudy className="commonIcons" />,
       heading: "Weather App",
       text: "In this Project i created one Weather Checking app using Weather API.",
       href: "https://react-weatther-app.netlify.app/",
     },
     {
-      id: 9,
+      id: 10,
       icon: <BsTextareaT className="commonIcons" />,
       heading: "Text Converter",
       text: "In this Project i developed one text converter with some functionality using Javascript Logic.",
