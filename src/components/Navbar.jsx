e469c38ac23b7e4ff59dc6c8e6f85531d217f0ff
Suraj Yadav1 <sarry7045@gmail.com>
2023-03-13 18:10:50 +0530

    Bug-Fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,35 +10,58 @@ const Navbar = () => {
         <NavLink to="/" className="logo" title="Suraj Yadav">
           {Portfolio}
         </NavLink>
+
+        {/* <a href="/" className="logo" title="Suraj Yadav">
+          {Portfolio}
+        </a> */}
         <input type="checkbox" id="click" />
         <label for="click" className="menu-btn">
           <i className="fa fa-bars"></i>
         </label>
         <ul>
           <li>
-            <NavLink className="active" title="About me" to="/">
+            {/* <NavLink className="active" title="About me" to="/">
               About Me
-            </NavLink>
+            </NavLink> */}
+
+            <a className="active" title="About me" href="/">
+              About Me
+            </a>
           </li>
           <li>
-            <NavLink to="/skills" title="Skills">
+            {/* <NavLink to="/skills" title="Skills">
               Skills
-            </NavLink>
+            </NavLink> */}
+            <a href="/skills" title="Skills">
+              Skills
+            </a>
           </li>
           <li>
-            <NavLink to="/projects" title="Projects">
+            {/* <NavLink to="/projects" title="Projects">
               Projects
-            </NavLink>
+            </NavLink> */}
+
+            <a href="/projects" title="Projects">
+              Projects
+            </a>
           </li>
           <li>
-            <NavLink to="/experience" title="Experience">
+            {/* <NavLink to="/experience" title="Experience">
               Experiences
-            </NavLink>
+            </NavLink> */}
+
+            <a href="/experience" title="Experience">
+              Experiences
+            </a>
           </li>
           <li>
-            <NavLink to="/contact" title="Contact">
+            {/* <NavLink to="/contact" title="Contact">
               Contact Me
-            </NavLink>
+            </NavLink> */}
+
+            <a href="/contact" title="Contact">
+              Contact Me
+            </a>
           </li>
         </ul>
       </nav>
